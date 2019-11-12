947a6a7fd15e0b81d78553a2fbc7273b8c1acd27
Bret Fisher <bret@bretfisher.com>
2019-11-12 07:03:50 -0500

    updated concepts, lighter background

---
+++ slides/buildone.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+set -e
+case "$1" in
+once)
+  ./index.py
+  ./markmaker.py kube-mastery.yml > kube-mastery.yml.html || { 
+    rm kube-mastery.yml.html
+    break
+  }
+  ;;
+
+forever)
+  set +e
+  # check if entr is installed
+  if ! command -v entr >/dev/null; then
+    echo >&2 "First install 'entr' with apt, brew, etc."
+    exit
+  fi
+  
+  # There is a weird bug in entr, at least on MacOS,
+  # where it doesn't restore the terminal to a clean
+  # state when exitting. So let's try to work around
+  # it with stty.
+  STTY=$(stty -g)
+  while true; do
+    find . | entr -d $0 once
+    STATUS=$?
+    case $STATUS in
+    2) echo "Directory has changed. Restarting.";;
+    130) echo "SIGINT or q pressed. Exiting."; break;;
+    *) echo "Weird exit code: $STATUS. Retrying in 1 second."; sleep 1;;
+    esac
+  done
+  stty $STTY
+  ;;
+
+*)
+  echo "$0 <once|forever>"
+  ;;
+esac
